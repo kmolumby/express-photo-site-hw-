3cd537dbf17cc225cfe6f13839b4c924d7e80aec
Kelly <kelly@Jerrys-MacBook-Pro.local>
2018-10-15 20:20:23 -0600

    new.ejs and index.ejs done

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', (req,res) => {
-    res.render('user/index.ejs')
-})
+const Users = require('../models/user')
 
+
+
+router.get('/', (req,res) => {
+  Users.find({}, (err, allUsers) => {
+          res.render('user/index.ejs', {
+              users: allUsers})        
+      });
+  
+})
 
 router.get('/new', (req,res) =>{
     res.render('user/new.ejs')
 })
+
+router.post('/', (req,res) => {
+    Users.create(req.body, (err,createdUser) => {
+        res.redirect('/user')
+    })
+})
+
+
+
+
 
 module.exports = router;
